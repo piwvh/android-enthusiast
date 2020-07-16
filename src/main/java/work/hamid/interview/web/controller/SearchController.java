1c52e8bc3fb2c5253bda63305c877abac066f7d1
Hamid Mohayeji <h.mohayeji@gmail.com>
2020-07-16 22:34:59 +0430

    implement endpoint to fetch 10 most voted questions

--- src/main/java/work/hamid/interview/web/controller/SearchController.java
+++ src/main/java/work/hamid/interview/web/controller/SearchController.java
@@ -24,11 +24,20 @@ public class SearchController {
 
     // return 10 newest android questions
     @GetMapping("/newest")
-    public SearchResult newestQuestions() {
+    public SearchResult newest() {
+        var params = new SearchParams();
+        params.setTagged(Collections.singletonList("android"));
+
+        return api.search(params);
+    }
+
+    // return 10 most voted android questions
+    @GetMapping("/most-voted")
+    public SearchResult mostVoted() {
         var params = new SearchParams();
         params.setFromDate(LocalDateTime.now().minusWeeks(1));
         params.setTagged(Collections.singletonList("android"));
-        params.setSort(SearchParams.Sort.CREATION);
+        params.setSort(SearchParams.Sort.VOTES);
 
         return api.search(params);
     }
